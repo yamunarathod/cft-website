bf803965a51b45f8012e7019990bdec6d9b2d7e3
Yamuna <yamunarathod779@gmail.com>
2024-08-12 19:03:20 +0530

    jnbv

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const cors = require('cors');
 const port = 3000;
+
+app.use(cors());  // Corrected this line
 
 app.use(express.static(path.join(__dirname, 'public')));
 
