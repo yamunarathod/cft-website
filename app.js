8ae481c0683002e88e42a2c2f979ad618e41f167
Yamuna <yamunarathod779@gmail.com>
2024-08-13 12:02:50 +0530

    kjhg

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const cors = require('cors');
+// const port = 3000;
+
+// const corsOptions = {
+//   origin: '*', 
+//   credentials: true 
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Define a route for the home page
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+
+// app.get("/contact-us", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "contact-us.html"));
+//   });
+
+//   app.get("/about", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "about.html"));
+//   });
+
+  
+//   app.get("/showcase-new", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "showcase-new.html"));
+//   });
+  
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -13,25 +53,34 @@ app.use(cors(corsOptions));
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Set the directory for EJS files
+app.set('views', path.join(__dirname, 'views'));
 
+// Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes and render EJS templates
+// Define routes to render EJS templates
 app.get("/", (req, res) => {
-  res.render("index"); // render index.ejs
+  res.render("index");
 });
 
 app.get("/contact-us", (req, res) => {
-  res.render("contact-us"); // render contact-us.ejs
+  res.render("contact-us");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about"); // render about.ejs
+  res.render("about");
 });
 
 app.get("/showcase-new", (req, res) => {
-  res.render("showcase-new"); // render showcase-new.ejs
+  res.render("showcase-new");
+});
+
+
+
+
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).render("404", { url: req.url });
 });
 
 // Start the server
